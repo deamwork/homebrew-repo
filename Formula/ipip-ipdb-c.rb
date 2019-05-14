7295ff6fc2e227d91d73890948171a2d49defbf7
Kosaka (CI) <admin@kosaka.moe>
2019-05-14 10:05:33 +0000

    Bump ipip-ipdb-c to 20190514

--- Formula/ipip-ipdb-c.rb
+++ Formula/ipip-ipdb-c.rb
@@ -2,12 +2,12 @@ class IpipIpdbC < Formula
   desc "IPIP.net IPDB query tool"
   homepage "https://github.com/ipipdotnet/ipdb-c"
   url "https://github.com/traviscross/mtr/archive/v0.92.tar.gz"
-  version "20190513"
+  version "20190514"
   sha256 "568a52911a8933496e60c88ac6fea12379469d7943feb9223f4337903e4bc164"
 
   bottle do
-    root_url "https://deb.kosaka.moe/bottles"
-    sha256 "3eb0c32e29633929867d88a6dd563c825642960313b7ca7484befed8af1dbfc0" => :mavericks
+    root_url "https://repo.kosaka.moe/bottles"
+    sha256 "a5b74050682df8d8cbfbc582270d14b150a0cdee6ddad2a1669316a05017043f" => :mavericks
   end
 
   depends_on "json-c"
